1b53caaade3b3762c886af83eb331fc4f1b5f33e
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-15 13:27:06 +0200

    feat: Implement controlled form inputs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,23 +21,38 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState(``);
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, pakced: false, id: Date.now() };
+    setDescription(``);
+    setQuantity(1);
   }
 
   return (
-    <div className="add-form" onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Add item" />
+      <input
+        type="text"
+        placeholder="Add item"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
 
@@ -64,7 +81,7 @@ function Item({ item }) {
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You are x% done with packing!</em>
     </footer>
   );
