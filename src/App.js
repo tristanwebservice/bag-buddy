7e42d80cad137ff7801a4dad5fb465e3eecf4c1b
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-15 16:02:53 +0200

    feat: Add dynamic item creation & state management

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,7 +20,7 @@ function Logo() {
   return <h1>Bag Buddy</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState(``);
   const [quantity, setQuantity] = useState(1);
 
@@ -28,6 +28,7 @@ function Form() {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, pakced: false, id: Date.now() };
+    onAddItems(newItem);
     setDescription(``);
     setQuantity(1);
   }
@@ -56,11 +57,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
