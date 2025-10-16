96f472be0d48cf2a754fffe8dd663043504c6a57
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-16 09:29:25 +0200

    feat: Implement clear list functionality

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ export default function App() {
     );
   }
 
+  function handleClearList(id) {
+    const confirmed = window.confirm(
+      `Are you sure you want to delete all items?`
+    );
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -27,6 +34,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -74,7 +82,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState(`packed`);
 
   let sortedItems = [...items];
@@ -109,6 +117,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear list</button>
       </div>
     </div>
   );
